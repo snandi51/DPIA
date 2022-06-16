9be93979dddcd89549b26fdb3ea08d0709b332dc
Capgemini2121 <vishal.ubale@capgemini.com>
2022-06-16 17:08:12 +0530

    added primary key as AutoField

--- Assessment/models.py
+++ Assessment/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Master(models.Model):
     # Login Models
-    primary_key = models.IntegerField(primary_key=True, null=False)
+    primary_key = models.AutoField(primary_key=True)
     username = models.CharField(max_length=30)
 
     # Organization details models
